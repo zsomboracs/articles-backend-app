7a08e4d52aed0b3c537e1aabec8dc55c7c040d76
Zsombor Acs <v-zsacs@expedia.com>
2019-10-21 11:51:12 +0200

    Skeleton Spring Boot app with Web, Data and PostgreSQL driver.

---
+++ src/test/java/com/apple/articles/ArticlesBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
